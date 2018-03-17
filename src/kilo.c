41475cf2207677e237bb57099cbf8217f7aef372
Unknown <mwbpenguin@gmail.com>
2018-03-17 10:42:17 +0800

    Added text drawing

--- src/kilo.c
+++ src/kilo.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <ctype.h>
 
@@ -38,17 +39,26 @@ enum editorKey {
 
 /*** data ***/
 
+// Data type for storing a row of text
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 // Struct to contain editor state
 struct editorConfig {
   // Cursor x and y position
   int cx, cy;
   int screenrows;
   int screencols;
+  int numrows;
+  erow row;
   // Original terminal attributes
   struct termios orig_termios;
 };
 
 struct editorConfig E;
+
 /*** terminal ***/
 
 // Prints error message and exits program
@@ -173,6 +183,19 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** file i/o ***/
+
+// Open and reading file from disk
+void editorOpen() {
+  char *line = "Hello, world!";
+  ssize_t linelen = 13;
+  E.row.size = linelen;
+  E.row.chars = malloc(linelen + 1);
+  memcpy(E.row.chars, line, linelen);
+  E.row.chars[linelen] = '\0';
+  E.numrows = 1;
+}
+
 /*** append buffer ***/
 
 struct abuf {
@@ -205,24 +228,32 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      // Print welcome message a third of the way down screen
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
-      // Center welcome message
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
+    // Check if currently draw row part of text buffer
+    if (y >= E.numrows) {
+      if (y == E.screenrows / 3) {
+        // Print welcome message a third of the way down screen
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        // Center welcome message
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        // Add welcome message to buffer
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--) abAppend(ab, " ", 1);
-      // Add welcome message to buffer
-      abAppend(ab, welcome, welcomelen);
     } else {
-      abAppend(ab, "~", 1);
+      // Truncate line if it goes past the end of screen
+      int len = E.row.size;
+      if (len > E.screencols) len = E.screencols;
+      // Write out chars field of erow to buffer
+      abAppend(ab, E.row.chars, len);
     }
-
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
@@ -323,6 +354,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.numrows = 0;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die ("getWindowSize");
 }
@@ -330,6 +362,7 @@ void initEditor() {
 int main() {
   enableRawMode();
   initEditor();
+  editorOpen();
 
   while (1) {
     editorRefreshScreen();
